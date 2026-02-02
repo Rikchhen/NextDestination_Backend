0d2a5ca96c49dca2e828e9f2592acf3e3ede65ba
Rikchhen <rikchhenlama3@gmail.com>
2026-02-02 19:47:28 +0545

    src/user/ added

---
+++ src/user/models/user.model.ts
@@ -0,0 +1,30 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  fullName: string;
+  email: string;
+  password: string;
+  phoneNumber: string;
+  role: "user" | "admin";
+  profilePicture?: string;
+}
+
+const UserSchema: Schema<IUser> = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    password: { type: String, required: true, select: false },
+    phoneNumber: { type: String },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+    profilePicture: { type: String },
+  },
+  { timestamps: true },
+);
+
+export const UserModel = mongoose.model<IUser>("User", UserSchema);
