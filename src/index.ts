9523315c728d6e6bb67601db09ef6e12f4450b3d
Rikchhen <rikchhenlama3@gmail.com>
2026-02-02 19:42:44 +0545

    src/index.ts modified

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,43 @@
-import express, { Application } from 'express';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import connectDB from './db/db';
-import userRouter from './routes/user.route';
-
-
+import express, { Application } from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { connectDatabase } from "./db/mongoose";
+import { PORT } from "./config";
+import userRouter from "./user/routes/user.route";
+import adminRouter from "./admin/route/admin.route";
+import path from "path";
+import cors from "cors";
 
 dotenv.config();
+console.log(process.env.PORT);
 
 const app: Application = express();
-const PORT = process.env.PORT;
 
-//database connection
-connectDB();
+let corsOptions = {
+  credentials: true,
+  origin: ["http://localhost:5000", "http://localhost:3000"],
+  // which url can access backend
+  // put your frontend domain/url here
+};
+// origin: "*", // yo sabai url lai access dinxa
+app.use(cors(corsOptions));
+
+app.use("/uploads", express.static(path.join(__dirname, "../uploads"))); // static file serving
 
 // Middleware
 app.use(bodyParser.json());
 
-// Register Routes
-app.use('/api/user', userRouter); 
+// Protected Admin Routes
+app.use("/api/admin", adminRouter);
+//User Routes
+app.use("/api/user", userRouter);
 
 // Start Server
-app.listen(PORT, () => {
+async function start() {
+  // database connection
+  await connectDatabase();
+  app.listen(PORT, () => {
     console.log(`Server running at: http://localhost:${PORT}`);
-});
+  });
+}
+start().catch((error) => console.log(error));
