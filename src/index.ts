08e47cf1b8d9c5847422dda0c55f0ee0e1f66167
Rikchhen <rikchhenlama3@gmail.com>
2026-02-13 16:05:57 +0545

    src/index.ts modified

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,10 @@
-import express, { Application } from "express";
-import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import { connectDatabase } from "./db/mongoose";
 import { PORT } from "./config";
-import userRouter from "./user/routes/user.route";
-import adminRouter from "./admin/route/admin.route";
-import path from "path";
-import cors from "cors";
+import app from "./app";
 
 dotenv.config();
 console.log(process.env.PORT);
-
-const app: Application = express();
-
-let corsOptions = {
-  credentials: true,
-  origin: ["http://localhost:5000", "http://localhost:3000"],
-  // which url can access backend
-  // put your frontend domain/url here
-};
-// origin: "*", // yo sabai url lai access dinxa
-app.use(cors(corsOptions));
-
-app.use("/uploads", express.static(path.join(__dirname, "../uploads"))); // static file serving
-
-// Middleware
-app.use(bodyParser.json());
-
-// Protected Admin Routes
-app.use("/api/admin", adminRouter);
-//User Routes
-app.use("/api/user", userRouter);
 
 // Start Server
 async function start() {
